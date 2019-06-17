995b7c7fffb77eb62b52932d849db690bb58bd0e
Lantz Warrick <betalantz@gmail.com>
2019-06-17 07:09:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,10 +2,10 @@ def line(deli_line)
   output = "The line is currently"
   if deli_line.size == 0
     output << " empty."
-    return output
+    puts output
   else
     output << ":"
     deli_line.each_with_index{|customer, index| output << " #{index + 1}. #{customer}"}
-    return output
+    puts output
   end
 end
